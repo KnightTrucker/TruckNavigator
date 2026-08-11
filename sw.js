180faef813dfc5cf0c4255d4eed13fb5e4e1bf33
KnightTrucker <mta.sec.comm@gmail.com>
2026-08-11 10:27:03 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+/* KnightTruckerNavy v46.1 */
+const CACHE='ktn-v46-1-shell';
+const LOCAL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
+  const url=new URL(req.url);
+  if(url.origin!==self.location.origin)return;
+
+  if(req.mode==='navigate' || url.pathname.endsWith('/index.html')){
+    event.respondWith(
+      fetch(req).then(resp=>{
+        const copy=resp.clone();
+        caches.open(CACHE).then(c=>c.put('./index.html',copy));
+        return resp;
+      }).catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(hit=>hit || fetch(req).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy));
+      return resp;
+    }))
+  );
+});
