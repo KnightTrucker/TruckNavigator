37a1d7c6e684e33a1cd9ecee953343f0db65de99
Andrea <mta.sec.comm@gmail.com>
2026-09-18 19:47:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* ToraNavy 0.17.32 MOTORWAY TRAFFIC GUIDANCE REPAIR */
-const CACHE='ktn-v01732-motorway-traffic-guidance-repair';
+/* ToraNavy 0.17.34 CANONICAL GUIDANCE STABILITY */
+const CACHE='ktn-v01734-canonical-guidance-stability';
 const LOCAL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
   './toranavy_startup.webp',
   './toranavy_logo.webp',
