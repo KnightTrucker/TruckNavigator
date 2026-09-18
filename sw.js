1d9e9aa0a20f9009e6915194226ffac62dc525be
Andrea <mta.sec.comm@gmail.com>
2026-09-18 07:11:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* ToraNavy 0.17.19 ROUTE-MAP ROUNDABOUT + STRUCTURAL SECOND */
-const CACHE='ktn-v01719a-route-map-round-second';
+/* ToraNavy 0.17.20 TIME + LANDSCAPE REPAIR */
+const CACHE='ktn-v01720a-time-landscape-repair';
 const LOCAL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
   './toranavy_startup.webp',
   './toranavy_logo.webp',
