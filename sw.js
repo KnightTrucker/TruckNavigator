41e745a9e6004f24380b84576c819c2bbcc18d28
Andrea <mta.sec.comm@gmail.com>
2026-09-20 15:02:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ToraNavy 0.17.50 MICROSCOPIO SAFE FINAL */
-const CACHE='ktn-v01751-report-weight-geocode';
+const CACHE='ktn-v01752-report-weight-geocode';
 const LOCAL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
   './toranavy_startup.webp',
   './toranavy_logo.webp',
