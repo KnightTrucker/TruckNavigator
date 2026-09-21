03bc7c5959d6defec27a4b042ed4c782eea3a0a5
Andrea <mta.sec.comm@gmail.com>
2026-09-21 14:56:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ToraNavy 0.17.50 MICROSCOPIO SAFE FINAL */
-const CACHE='ktn-v01770-ktn716-exit-entry-diagnostics';
+const CACHE='ktn-v01771-recovery-field-safe';
 const LOCAL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
   './toranavy_startup.webp',
   './toranavy_logo.webp',
