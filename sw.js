b644f3282836783829cc53c5984c82fd8da064e4
Andrea <mta.sec.comm@gmail.com>
2026-09-21 20:22:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* ToraNavy 0.17.78 MOTORWAY BIFURCATION / LANE SPLIT */
+/* ToraNavy 0.17.79 LANDSCAPE UI OPTIMIZATION */
 /* ToraNavy 0.17.50 MICROSCOPIO SAFE FINAL */
-const CACHE='ktn-v01778-motorway-bifurcation-lanes';
+const CACHE='ktn-v01779-landscape-ui';
 const LOCAL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
   './toranavy_startup.webp',
   './toranavy_logo.webp',
